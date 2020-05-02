c9e49c4b0d5fa5ba159369c89efe764f36879ece
Claude E David <claudedavid@gmail.com>
2020-05-02 14:00:25 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,25 @@
-from flask import Flask, render_template      
+import numpy as np
+from flask import Flask, render_template, jsonify 
+import pickle
 
 app = Flask(__name__)
+
+# Load the model
+with open('recommendation_CB.pickle', 'rb') as f:
+    model = pickle.load(f)
+    
+@app.route('/api',methods=['POST'])
+def predict():
+    # Get the data from the POST request.
+    data = request.get_json(force=True)
+
+    # Make prediction using model loaded from disk as per the data.
+    prediction = model.predict([[np.array(data['exp'])]])
+
+    # Take the first value of prediction
+    output = prediction[0]
+
+    return jsonify(output)
 
 @app.route("/")
 def home():
